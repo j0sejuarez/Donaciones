7f1e5162f6f5c517440a4c3bb3e8372f58268915
LSebastian-Mtz <119455889+LSebastian-Mtz@users.noreply.github.com>
2024-06-06 00:10:21 -0700

    Add files via upload

--- donaciones_bd/index.php
+++ donaciones_bd/index.php
@@ -4,28 +4,67 @@
         <title>Donaciones</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="paginas_basic.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="css/s_index.css">
     </head>
     <body>
-    <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="#">WebSiteName</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="p_alta_donacion.php">Donaciones</a></li>
-                <li><a href="#">Consulta</a></li>
-            </ul>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="#">WebSiteName</a>
+                </div>
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">Home</a></li>
+                    <li><a href="p_donaciones.php">Donaciones</a></li>
+                    <li><a href="#">Consulta</a></li>
+                </ul>
         
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="p_login.php"><span class="glyphicon glyphicon-user"></span> Login</a></li>
-                <li><a href="p_alta_donante.php"><span class="glyphicon glyphicon-log-in"></span> Registro</a></li>
-            </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="p_login.php"><span class="glyphicon glyphicon-user"></span> Login</a></li>
+                    <li><a href="p_alta_donante.php"><span class="glyphicon glyphicon-log-in"></span> Registro</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="bloque">
+            <div class="parte1">
+            
+            </div>
+            <div class="parte2">
+                <p>
+                    Bienvenido a nuestra página web dedicada a donaciones monetarias, materiales y alimentos. Aquí, puedes encontrar formas sencillas y efectivas de apoyar a aquellos que más lo necesitan. Inicia sesión para comenzar a hacer la diferencia en el mundo.
+                </p>
+                <form id="form">
+                    <input type="submit" name="login" formaction="p_login.php" value="Iniciar sesión">
+                </form>
+            </div>
+            <div class="parte3">
+            
+            </div>
+            <div class="parte4">
+                
+            </div>
+            <div class="parte5">
+            
+            </div>
         </div>
-    </nav>
+        <hr class="rounded">
+        <div class="bloque2">
+            <div class="parte6">
+                
+            </div>
+            <div class="parte7">
+                <h1>
+                    ¿Quienes Somos?
+                </h1>
+                <p>
+                    Somos una organización sin fines de lucro dedicada a luchar contra el hambre y la pobreza en nuestra comunidad. Desde nuestra fundación en 2003, hemos trabajado incansablemente para brindar asistencia alimentaria y material a las personas más necesitadas, con el objetivo de mejorar su calidad de vida y promover la justicia social.
+                </p>
+            </div>
+        </div>
+        <footer>
+            <p class="p_footer">© 2024 Donaciones UABC</p>
+        </footer>        
     </body>
 </html>
--- donaciones_bd/p_alta_donante.php
+++ donaciones_bd/p_alta_donante.php
@@ -4,34 +4,25 @@
         <title>Registro de donantes</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="paginas_basic.css">
+        <link rel="stylesheet" href="css/s_alta_donante.css">
     </head>
     <body>
         <div id="bloque">
-            <h1>Registrar donante</h1>
-            <form method="post" action="alta_donante.php">
-                <p>
-                <p><label for='nombre_d'>Nombre</label></p>
-                <input class="int" type ="text" id ="nombre_d" name="nombre_d" required>
-                <p>
-                <p><label for='ap_pat_d'>Apellido Paterno</label></p>
-                <input class="int" type="text" id ="ap_pat_d" name="ap_pat_d" required>
-                <p>
-                <p><label for='ap_mat_d'>Apellido Materno</label></p>
-                <input class="int" type ="text" id ="ap_mat_d" name="ap_mat_d">
-                <p>
-                <p><label for='telefono_d'>Telefono</label></p>
-                <input class="int" type ="number" id ="telefono_d" name="telefono_d" required>
-                <p>
-                <p><label for='correo_d'>Correo</label></p>
-                <input class="int" type="text" id ="correo_d" name="correo_d" required>
-                <p>
-                <p><label for='pwd_d'>Contraseña</label></p>
-                <input class="int" type ="password" id ="pwd_d" name="pwd_d" required>
-                <p>
+            <div class="parte1">
+                <img src="hands-holding-hearts.svg" alt="Italian Trulli" class="img">
+            </div>
+            <div class="parte2">
+                <h1>Registrar donante</h1>
+                <form method="post" action="alta_donante.php" id=form>
+                    <input class="int" type ="text" id ="nombre_d" name="nombre_d" required placeholder="Nombre"><br>
+                    <input class="int" type="text" id ="ap_pat_d" name="ap_pat_d" required placeholder="Apellido Paterno"><br>
+                    <input class="int" type ="text" id ="ap_mat_d" name="ap_mat_d" placeholder="Apellido Materno"><br>
+                    <input class="int" type ="number" id ="telefono_d" name="telefono_d" required placeholder="Teléfono"><br>
+                    <input class="int" type="text" id ="correo_d" name="correo_d" required placeholder="Correo Electrónico"><br>
+                    <input class="int" type ="password" id ="pwd_d" name="pwd_d" required placeholder="Contraseña"><br>
                     <input class="boton" type="submit" value="Registrar">
-                </p>
             </form>
+            </div>
         </div>
     </body>
 </html>
